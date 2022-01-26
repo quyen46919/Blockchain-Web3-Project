083fdabbda140c25da1305e484c9530bed858c9b
Nguyen Chau Quyen <69032266+quyen46919@users.noreply.github.com>
2022-01-26 23:48:48 +0700

    update smart contract

--- contracts/ItemManager.sol
+++ contracts/ItemManager.sol
@@ -16,31 +16,47 @@ contract ItemManager is Ownable {
         string _identifier;
         uint256 _itemPrice;
         ItemManager.SupplyChainState _state;
+        string _otherInfos;
     }
 
     mapping(uint256 => S_Item) public items;
     uint256 itemIndex;
+
+    // EVENTS
     event SupplyChainStep(
         uint256 _itemIndex,
         uint256 _step,
-        address _itemAddress
+        address _itemAddress,
+        string _otherInfos
     );
 
-    function createItem(address _owner, string memory _identifier, uint256 _itemPrice)
+    event UpdatedItemInfo(
+        uint256 _itemIndex,
+        address _itemAddress,
+        string _otherInfos
+    );
+
+
+    // FUNCTIONS
+    function createItem(address owner, string memory identifier, uint256 itemPrice, string memory otherInfos)
         public
         onlyOwner
     {
-        Item item = new Item(this, _itemPrice, itemIndex);
-        items[itemIndex]._owner = _owner;
+        Item item = new Item(this, itemPrice, itemIndex);
+        items[itemIndex]._owner = owner;
         items[itemIndex]._item = item;
-        items[itemIndex]._identifier = _identifier;
-        items[itemIndex]._itemPrice = _itemPrice;
+        items[itemIndex]._identifier = identifier;
+        items[itemIndex]._itemPrice = itemPrice;
         items[itemIndex]._state = SupplyChainState.Created;
+        items[itemIndex]._otherInfos = otherInfos;
+
         emit SupplyChainStep(
             itemIndex,
             uint256(items[itemIndex]._state),
-            address(item)
+            address(item),
+            otherInfos
         );
+
         itemIndex++;
     }
 
@@ -54,10 +70,12 @@ contract ItemManager is Ownable {
             "Item is further in the chain!"
         );
         items[_itemIndex]._state = SupplyChainState.Paid;
+    
         emit SupplyChainStep(
             _itemIndex,
             uint256(items[_itemIndex]._state),
-            address(items[_itemIndex]._item)
+            address(items[_itemIndex]._item),
+            items[_itemIndex]._otherInfos
         );
     }
 
@@ -67,10 +85,36 @@ contract ItemManager is Ownable {
             "Item is further in the chain!"
         );
         items[_itemIndex]._state = SupplyChainState.Delivered;
+
         emit SupplyChainStep(
             _itemIndex,
             uint256(items[_itemIndex]._state),
-            address(items[_itemIndex]._item)
+            address(items[_itemIndex]._item),
+            items[_itemIndex]._otherInfos
+        );
+    }
+
+    function compareStrings(string memory a, string memory b) internal returns (bool) {
+        return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
+    }
+
+    function updateInfos(uint256 _itemIndex, string memory infos) public onlyOwner {
+        require(
+            items[_itemIndex]._state != SupplyChainState.Paid,
+            "Paid item cannot update info!"
+        );
+
+        require(
+            compareStrings(items[_itemIndex]._otherInfos, infos) == false,
+            "Nothing change to update!"
+        );
+
+        items[_itemIndex]._otherInfos = infos;
+
+        emit UpdatedItemInfo(
+            _itemIndex,
+            address(items[_itemIndex]._item),
+            items[_itemIndex]._otherInfos
         );
     }
 }
